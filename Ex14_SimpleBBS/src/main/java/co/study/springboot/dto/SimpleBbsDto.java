2c22dc0670462fb6d68add63ce4cbe29b0e26b2e
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-01-18 00:54:29 +0900

    0117 간단한 게시판 만들기

---
+++ Ex14_SimpleBBS/src/main/java/co/study/springboot/dto/SimpleBbsDto.java
@@ -0,0 +1,11 @@
+package co.study.springboot.dto;
+
+import lombok.Data;
+
+@Data
+public class SimpleBbsDto {
+	private int id;
+	private String writer;
+	private String title;
+	private String content;
+}
