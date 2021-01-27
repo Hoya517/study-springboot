085076b13593ac371b0beb70e6917e17829ca2ea
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-01-28 00:02:54 +0900

    수정

--- Ex25_Security/src/main/java/com/study/springboot/auth/WebSecurityConfig.java
+++ Ex25_Security/src/main/java/com/study/springboot/auth/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.study.springboot.java;
+package com.study.springboot.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
